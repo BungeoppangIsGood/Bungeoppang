df1e7a5a0c95c82862806a4d4d65b96cf1f7f454
leechaeyeon321 <leechaeyeon321@gmail.com>
2022-12-12 16:20:36 +0900

    Feat: shopdetail file upload

---
+++ 221209/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+
+app.use("/img", express.static("img"));
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.render("shopdetail");
+});
+
+app.listen(port, ()=> {
+    console.log("server open: ", port);
+})
