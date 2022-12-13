c848944cdbe9c1e8ed5a1d1eb4a6c9e5e712acac
dolahee <155641@naver.com>
2022-12-13 18:40:44 +0900

    feat: 검색창

--- 221209/index.js
+++ 221209/index.js
@@ -2,20 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 app.use("/img", express.static("img"));
-app.use(express.urlencoded({extended: true})); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-    res.render("shopdetail");
+  res.render("shopdetail");
 });
 
 app.get("/register", function (req, res) {
-    res.render("shopregister");
+  res.render("shopregister");
 });
 
-app.listen(port, ()=> {
-    console.log("server open: ", port);
-})
+app.listen(port, () => {
+  console.log("server open: ", port);
+});
