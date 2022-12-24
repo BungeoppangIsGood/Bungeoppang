912e7ee1eb7502a6adff333119a3e46e417ad127
금서하 <048965s@naver.com>
2022-12-24 15:59:59 +0900

    feat: 221224 css, logout

--- 221209/static/js/mypage.js
+++ 221209/static/js/mypage.js
@@ -11,7 +11,21 @@ function myPage() {
 }
 
 function logOut() {
-    location.href = "/main";
+    const con = confirm("로그아웃 하시겠습니까?")
+    if (con === true) {
+        location.href = "/auth/logout";
+        alert("로그아웃 성공");
+    }
+    // axios({
+    //     method: "delete",
+    //     url: "/auth/logout",
+    // }).then((data) => {
+    //     if(data === true) {
+    //         document.location.href = "/auth/delete";
+    //     } else {
+    //         alert("알 수 없는 오류 입니다");
+    //     }
+    // });
 }
 
 function back() {
